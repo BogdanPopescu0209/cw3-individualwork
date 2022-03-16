a50a627ccd813c029a6422506f7e4e8acc41a978
BogdanPopescu0209 <59745574+BogdanPopescu0209@users.noreply.github.com>
2022-03-16 22:08:21 +0000

    add bootstrap and output lessons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,47 @@
 <template>
   <div id="app">
-   
+    <div class="container-fluid">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg"></div>
+
+          <div class="col-lg">
+            <header>
+              <h1>{{ sitename }}</h1>
+
+              <div class="row">
+                <button
+                  v-if="cart.length > 0"
+                  class="checkout"
+                  v-on:click="showCheckout"
+                >
+                  {{ cart.length }}
+                  <span class="fas fa-cart-plus"></span> Checkout
+                </button>
+              </div>
+              <input
+                class="search"
+                type="text"
+                v-model="searchItem"
+                placeholder="Search..."
+              />
+
+              <button class="searchBtn">
+                <span class="fas fa-search"></span>Search
+              </button>
+            </header>
+          </div>
+
+          <div class="col-lg"></div>
+        </div>
+
+        <br />
+
+        <div class="row">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,12 +62,9 @@ export default {
     };
   },
 
-  methods: {
-   
-  }
+  methods: {},
 };
 </script>
 
 <style>
-
 </style>
