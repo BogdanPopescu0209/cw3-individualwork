81c3d02d74459790fb2666247fa1e31bb977eca1
BogdanPopescu0209 <59745574+BogdanPopescu0209@users.noreply.github.com>
2022-03-16 22:07:14 +0000

    Initialize vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+import products from "./components/porducts.vue";
+import checkout from "./components/checkout.vue";
+
+export default {
+  name: "App",
+  components: { products, checkout },
+  data() {
+    return {
+      sitename: "After School Club",
+      cart: [],
+      products: [],
+      showProduct: true,
+      searchItem: "",
+    };
+  },
+
+  methods: {
+   
+  }
+};
+</script>
+
+<style>
+
+</style>
